773a2b11795911a72b0785ef151dabd13913936c
Ryan Lee <ryanyinlee@protonmail.com>
2022-01-24 06:23:09 -0800

    all tests passing

--- linked-list/__tests__/index.test.js
+++ linked-list/__tests__/index.test.js
@@ -238,3 +238,18 @@ xdescribe("“Happy Path” where k is not at the end, but somewhere in the midd
     expect(list.findAtKth(-3)).toEqual("b");
   });
 });
+
+// Can successfully push onto a stack
+// Can successfully push multiple values onto a stack
+// Can successfully pop off the stack
+// Can successfully empty a stack after multiple pops
+// Can successfully peek the next item on the stack
+// Can successfully instantiate an empty stack
+// Calling pop or peek on empty stack raises exception
+// Can successfully enqueue into a queue
+// Can successfully enqueue multiple values into a queue
+// Can successfully dequeue out of a queue the expected value
+// Can successfully peek into a queue, seeing the expected value
+// Can successfully empty a queue after multiple dequeues
+// Can successfully instantiate an empty queue
+// Calling dequeue or peek on empty queue raises exception
