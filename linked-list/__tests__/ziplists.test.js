7b4c07ce979353e77b464e94af7cf57cbce08a71
Ryan Lee <ryanyinlee@protonmail.com>
2022-01-24 05:36:18 -0800

    all tests passing

--- linked-list/__tests__/ziplists.test.js
+++ linked-list/__tests__/ziplists.test.js
@@ -5,38 +5,60 @@ const LinkedList = require('../ziplists.js');
 // “Happy Path” - Expected outcome
 describe("“Happy Path” - Expected outcome", () => {
     it("Linked lists should combine.", () => {
-      let list = new LinkedList();
-  
-      list.add();
-      list.add();
-      list.add();
-      
-      expect(list.head.value).toEqual();
+        let list1 = new LinkedList();
+        let list2 = new LinkedList();
+        
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        
+        list2.add(5);
+        list2.add(9);
+        list2.add(4);
+
+        let list3 = new LinkedList();
+        let returnList = list3.merge(list1, list2);
+        expect(returnList.toString()).toEqual("{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> NULL");
     });
   });
 
 // Expected failure
 describe("Expected failure", () => {
     it("Linked list should not combine.", () => {
-      let list = new LinkedList();
-  
-      list.add();
-      list.add();
-      list.add();
+        let list1 = new LinkedList();
+        let list2 = new LinkedList();
+        
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        let list3 = new LinkedList();
+        let returnList = list3.merge(list1, list2);
+        
+        
       
-      expect(list.head.value).toEqual();
+        expect(returnList.toString()).toEqual("{ 1 } -> { 3 } -> { 2 } -> NULL");
     });
   });
 
 // Edge Case (if applicable/obvious)
 describe("Edge Case (if applicable/obvious)", () => {
     it("Linked lists are different lengths.", () => {
-      let list = new LinkedList();
-  
-      list.add();
-      list.add();
-      list.add();
-      
-      expect(list.head.value).toEqual();
+        let list1 = new LinkedList();
+        let list2 = new LinkedList();
+        
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        list1.add(7);
+        
+        list2.add(5);
+        list2.add(9);
+        list2.add(4);
+        
+
+
+        let list3 = new LinkedList();
+        let returnList = list3.merge(list1, list2);
+        expect(returnList.toString()).toEqual("{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> { 7 } -> NULL");
     });
   });
--- linked-list/ziplists.js
+++ linked-list/ziplists.js
@@ -31,44 +31,58 @@ class LinkedList {
         this.size+=1;
       }
 
+      toString() {
+        let currentNode = this.head;
+        let returnString = "";
+    
+        while (currentNode) {
+          returnString += "{ " + currentNode.value + " } -> ";
+          currentNode = currentNode.next;
+        }
+    
+        returnString += "NULL";
+    
+        return returnString;
+      }
 
-
-}
-// merge function must be outside of the linkedlist
-function merge(list1, list2) {
+        // merge function must be outside of the linkedlist?
+        // maybe not
+        // realize it will be easier to just return list1 rather than make a new list
+        // help with logic from daniel-jacks
+    merge(list1, list2) {
     // using 1,3,2 and 5,9,4
     let list1current = list1.head; // head is value: 1
     let list2current = list2.head; // head is value: 5
-    let list1next, list2next; // set these as empty placeholders
+    let list1holder, list2holder; // set these as empty placeholders
 
     while (list1current != null && list2current != null){ // both of these are not null
         // musical chairs
 
-        // must grab head of list 1
-        
+        //list1holder grabs list1current.next which is 3
+        list1holder = list1current.next;
 
+        // we can now change list1current.next to what list2current's head was: 5
+        list1current.next = list2current; 
+        // list1current is list1.head, which is 1. Now list1current is 3.
+        list1current = list1holder; 
+        // set empty list2holder to list2current.next which is: 9
+        list2holder = list2current.next;
+        // set list2current.next which was 9, to list1current which is 3;
+        list2current.next = list1current;
+        // setlist2current, which is list2.head, or 5, to: 9.
+        list2current = list2holder;
 
-
-
-
-
-
-
-        list1next = list1current.next;
-        list2next = list2current.next;
-
-        list1current.next = list1next;
-        list2current.next = list2current;
-
-        list1current = list1next;
-        list2current = list2next;
+    
     }
 
-    list1 = list1current;
+    
     return list1;
 
 
 }
+
+}
+
 
 let list1 = new LinkedList();
 let list2 = new LinkedList();
@@ -85,6 +99,10 @@ list2.add(4);
 
 console.log(JSON.stringify(list2));
 
-let list3 = merge(list1, list2);
+let list3 = new LinkedList();
+let returnList = list3.merge(list1, list2);
 
-console.log(list3);
+console.log(JSON.stringify(returnList));
+
+
+module.exports = LinkedList;
