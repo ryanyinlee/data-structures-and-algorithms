4880165cb6283dc553988b178177d384e19282d3
Ryan Lee <ryanyinlee@protonmail.com>
2022-02-02 04:03:14 -0800

    all tests passing

---
+++ javascript/trees/__tests__/tree-breadth-first.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const { BinaryTree, Node,} = require('../tree-breadth-first/tree-breadth-first.js');
+
+// “Happy Path” - Expected outcome
+describe("“Happy Path” - Expected outcome", () => {
+    it("Finds the correct maxValue of 11 in an unsorted tree", () => {
+        const tree = new BinaryTree();
+let node1 = new Node(2);
+let node2 = new Node(7);
+let node3 = new Node(5);
+let node4 = new Node(2);
+let node5 = new Node(6);
+let node6 = new Node(5);
+let node7 = new Node(11);
+let node8 = new Node(9);
+let node9 = new Node(4);
+
+tree.root = node1; //2
+tree.root.left = node2; //7
+tree.root.right = node3; //5
+tree.root.left.left = node4; //2
+tree.root.left.right = node5; //6
+tree.root.left.right.left = node6 //5
+tree.root.left.right.right = node7 //11
+tree.root.right.right = node8; //9
+tree.root.right.right.left = node9; //4
+
+let result = tree.breadthFirst();
+       
+    expect(result).toEqual([2,7, 5, 2, 6, 9, 5, 11, 4]);
+    });
+  });
+
+// Expected failure
+describe("Expected failure", () => {
+    it("Expected failure", () => {
+        let tree = new BinaryTree();
+        let result = tree.breadthFirst();
+        expect(result).toEqual(undefined);
+    });
+  });
+
+
+// Edge Case (if applicable/obvious)
+describe("Edge Case (if applicable/obvious)", () => {
+    it("Edge Case (if applicable/obvious)", () => {
+        const tree = new BinaryTree();
+        let node1 = new Node(-2);
+        let node2 = new Node(-7);
+        let node3 = new Node(-5);
+        let node4 = new Node(-2);
+        let node5 = new Node(-6);
+
+        
+        tree.root = node1; //2
+        tree.root.right = node2; //7
+        tree.root.right.right = node3; //5
+        tree.root.right.right.right = node4; //2
+        tree.root.right.right.right.right = node5; //6
+  
+        expect(tree.breadthFirst()).toEqual([-2, -7, -5, -2, -6]);
+    });
+  });
---
+++ javascript/trees/tree-breadth-first/README.md
@@ -0,0 +1,36 @@
+# tree-breadth-first
+Write a function called breadth first
+Arguments: tree
+Return: list of all values in the tree, in the order they were encountered
+NOTE: Traverse the input tree using a Breadth-first approach
+
+Breadth-first means:
+
+1) All the nodes on the level
+2) From left to right
+
+The example from the board shows the following (non-sorted) Binary Tree
+
+            2
+    7               5
+2       6               9
+    5       11      4
+
+Output: [2,7,5,2,6,9,5,11,4]
+
+## Whiteboard Process
+![](codechallenge17uml.PNG)
+
+## Approach & Efficiency
+Time: 
+
+
+Space: Linear? As long as the 
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+
+## Credits
+
+Udemy: JavaScript Algorithms and Data Structures Masterclass
+by Colt Steele
