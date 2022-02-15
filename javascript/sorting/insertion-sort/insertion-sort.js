5fb5486391e12d2915dfdda9333916598f3e796a
Ryan Lee <ryanyinlee@protonmail.com>
2022-02-15 02:33:30 -0800

    all tests passing, code looks like pseudo

---
+++ javascript/sorting/insertion-sort/insertion-sort.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let arrtest = [20,18,12,8,5,-2];
+
+function insertionSort(arr) {
+
+
+    for (let i = 1; i < arr.length; i+=1) {
+        let j = i - 1;
+        let temp = arr[i];
+     
+        while (j >= 0 && temp < arr[j]) {
+            arr[j+1] = arr[j];
+            j -= 1;
+        }
+    
+        arr[j+1] = temp;
+    
+    }
+    
+    return arr;
+    
+    }
+
+  let result = insertionSort(arrtest);
+
+    console.log(result)
+
+module.exports = insertionSort;
