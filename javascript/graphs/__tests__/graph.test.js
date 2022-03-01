920c6829700fa1bf3f6a61af865a00bb0b7a3f95
Ryan Lee <ryanyinlee@protonmail.com>
2022-03-01 02:17:33 -0800

    method added, test passing

--- javascript/graphs/__tests__/graph.test.js
+++ javascript/graphs/__tests__/graph.test.js
@@ -83,6 +83,21 @@ describe("Graph Testing", () => {
         expect(returnValue).toEqual(null);
       });
 
+
+      it("Breadth first traversal functionas it should.", () => {
+        const graph = new Graph();  
+        graph.add('Seattle');
+        graph.add('New York');
+        graph.add('Palo Alto');
+        graph.add('San Francisco');
+        graph.addEdge('Seattle', 'Palo Alto');
+        graph.addEdge('Seattle', 'New York');
+        graph.addEdge('Seattle', 'San Francisco');
+        let returnValue = graph.breadthFirst('Seattle');
+
+        expect(returnValue).toEqual([ 'Seattle', 'Palo Alto', 'New York', 'San Francisco' ]);
+      });
+
   });
 
 
--- javascript/graphs/graph.js
+++ javascript/graphs/graph.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { resetDefaultConfig } = require("eslint/lib/rule-tester/rule-tester");
+
 // class Vertex {
 //     constructor(value) {
 //       this.value = value; 
@@ -82,26 +84,71 @@ size () {
 // Return: A collection of nodes in the order they were visited.
 // Display the collection
 breadthFirst(node){
+let queue = [node];
 let collection = [];
+let nodesVisited = {};
+nodesVisited[node] = true;
+let currentNode;
+
+while(queue.length){
+    currentNode = queue.shift();
+    // console.log("current node", currentNode);
+    collection.push(currentNode);
+    // console.log('collection', collection)
+    //console.log('this.adjacencylist', Array.from(this.adjacencyList.get(currentNode)));
+    Array.from(this.adjacencyList.get(currentNode)).forEach(neighbor => {
+        if(!nodesVisited[neighbor]){
+            //console.log('nodesVisited = ', nodesVisited);
+            nodesVisited[neighbor] = true;
+            queue.push(neighbor);
+        }  
+
+    });
+}
 
 return collection;
 }
 
 }
 
-// let graph = new Graph();
+let graph = new Graph();
 
-// graph.add('Seattle');
-// graph.add('New York');
-// graph.add('Palo Alto');
-// graph.add('San Francisco');
-//console.log("get the nodes", graph.getNodes());
-//console.log(graph);
-// graph.addEdge('Seattle', 'Palo Alto');
-// graph.addEdge('Seattle', 'New York');
-// graph.addEdge('Seattle', 'San Francisco');
-// graph.getNeighbors('Palo Alto');
-// graph.getNeighbors('Seattle');
-//console.log('graph size is ', graph.size());
+graph.add('Seattle');
+graph.add('New York');
+graph.add('Palo Alto');
+graph.add('San Francisco');
+console.log("get the nodes", graph.getNodes());
+console.log(graph);
+graph.addEdge('Seattle', 'Palo Alto');
+graph.addEdge('Seattle', 'New York');
+graph.addEdge('Seattle', 'San Francisco');
+graph.getNeighbors('Palo Alto');
+graph.getNeighbors('Seattle');
+console.log('graph size is ', graph.size());
+
+let result = graph.breadthFirst('Seattle');
+console.log(result);
 
 module.exports = Graph;
+
+
+// let g = new Graph();
+
+// g.add("A");
+// g.add("B");
+// g.add("C");
+// g.add("D");
+// g.add("E");
+// g.add("F");
+
+
+// g.addEdge("A","B");
+// g.addEdge("A","C");
+// g.addEdge("B","D");
+// g.addEdge("C","E");
+// g.addEdge("D","E");
+// g.addEdge("D","F");
+// g.addEdge("E","F");
+
+// let result = g.breadthFirst("A");
+// console.log(result);
